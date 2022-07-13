b549b40bf4f479a18a041470bff7fb943b9126de
Nadine101 <nadinet026@gmail.com>
2022-07-12 21:46:46 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let item_names = ['beer','candy','chocolate'];
+let it_price = [12,2,5]
+let in_stock = [true,false,true];
+let first_item_names = ['beer'];
+let first_item_price = [12];
+item_names.push('treats');
+it_price.push(8);
+in_stock.push(fales,true,false);
+let last_item_name = item_names.pop();
+let last_item_price = it_price.pop();
+let last_item_stock = in_stock.pop();
